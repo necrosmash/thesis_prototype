b6b3638480a9ffd00d32c191cf8cce5bef606160
Rob Drury <robjdrewry@gmail.com>
2023-03-02 13:39:41 +0100

    chatGPT support
    - choose model by inspecting the `OpenAiApi` prefab in the Inspector
    - Adds a new abstract class, `APIResponse`, for dealing with response data shared by ChatGPT and DaVinci

--- Assets/Scripts/ChatGPTResponse.cs
+++ Assets/Scripts/ChatGPTResponse.cs
@@ -1,29 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.VersionControl;
 using UnityEngine;
 
 [System.Serializable]
-public class DaVinciResponse : APIResponse
+public class ChatGPTResponse : APIResponse
 {
     public Choice[] choices;
 
     public override void ParseBattleInfo()
     {
-        // choices[0] contains "text", "index", etc.
+        // choices[0] contains "message"
         if (choices[0] == null)
         {
             throw new BattleInfoNotFoundException();
         }
 
-        battleInfo = JsonUtility.FromJson<BattleInfo>(choices[0].text);
+        battleInfo = JsonUtility.FromJson<BattleInfo>(choices[0].message.content);
     }
 
     [System.Serializable]
     public class Choice
     {
-        public string text;
+        public Message message;
         public int index;
-        public string logprobs;
         public string finish_reason;
+    }
+
+    [System.Serializable]
+    public class Message
+    {
+        public string role;
+        public string content;
     }
 }
--- Assets/Scripts/OpenAiApi.cs
+++ Assets/Scripts/OpenAiApi.cs
@@ -1,30 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq.Expressions;
 using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.Networking;
 
 public class OpenAiApi : MonoBehaviour
 {
-    DaVinciResponse dvr;
+    APIResponse response;
     string prompt;
+
+    public Model model;
+
+    public string daVinciUrl = "https://api.openai.com/v1/completions";
+    public string chatGptUrl = "https://api.openai.com/v1/chat/completions";
+
+    public enum Model
+    {
+        davinci,
+        chatgpt
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         prompt = Regex.Replace("I want you to return me a JSON object. All of your output should be a part of the JSON object. Do not output any text except for the JSON object. Here is the example of the JSON object: " + JsonUtility.ToJson(new BattleInfo()) + " \"weapon\" must be one of the following values: \"sword\", \"hammer\", \"bow\". It cannot be anything else. \"size\" must be one of the following values: \"small\", \"medium\", \"large\". It cannot be anything else. Orc names must also include a descriptor starting with \"the\", such as \"Uzguk the Undefeated\". Be creative about these descriptors. Describe the opening scene in the \"openingScene\" string. The opening scene must be a story about an elf about to engage in a battle with a group of orcs. Be creative when you come up with descriptions of the orcs. Populate the \"orcs\" array based on the opening scene. ", "\"", "\\\"");
-        StartCoroutine(Post());
+        Post(model);
     }
 
-    IEnumerator Post()
+    private void Post(Model m)
     {
-        // hardcode values, should be configurable?
-        // could just be through the inspector.
-        string postData = "{\"model\": \"text-davinci-003\", \"prompt\": \"" + prompt + "\", \"temperature\": 0.7, \"max_tokens\": 3000}";
-        UnityWebRequest request = UnityWebRequest.Post("https://api.openai.com/v1/completions", postData, "application/json");
+        string davinciPostData = "{\"model\": \"text-davinci-003\", \"prompt\": \"" + prompt + "\", \"temperature\": 0.7, \"max_tokens\": 3000}";
+        string chatgptPostData = "{\"model\": \"gpt-3.5-turbo\", \"messages\": [{\"role\": \"user\", \"content\": \"" + prompt + "\"}]}";
+
+        switch (m)
+        {
+            case Model.chatgpt:
+                StartCoroutine(Post(GenerateRequest(chatGptUrl, chatgptPostData)));
+                break;
+            default:
+            case Model.davinci:
+                StartCoroutine(Post(GenerateRequest(daVinciUrl, davinciPostData)));
+                break;
+        }
+    }
+
+    private UnityWebRequest GenerateRequest(string url, string postData)
+    {
+        UnityWebRequest request = UnityWebRequest.Post(url, postData, "application/json");
         request.SetRequestHeader("Authorization", "Bearer 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");
 
+        return request;
+    }
+
+    private IEnumerator Post(UnityWebRequest request)
+    {
         yield return request.SendWebRequest();
         if (request.result != UnityWebRequest.Result.Success)
         {
@@ -32,8 +61,14 @@ public class OpenAiApi : MonoBehaviour
         }
         else
         {
-            dvr = JsonUtility.FromJson<DaVinciResponse>(request.downloadHandler.text);
-            dvr.ParseBattleInfo();
+            if (model.Equals(Model.davinci))
+                response = JsonUtility.FromJson<DaVinciResponse>(request.downloadHandler.text);
+            else
+                response = JsonUtility.FromJson<ChatGPTResponse>(request.downloadHandler.text);
+
+            response.ParseBattleInfo();
+            Debug.Log("API call complete");
+            Debug.Log(response.battleInfo.openingScene);
         }
     }
 
