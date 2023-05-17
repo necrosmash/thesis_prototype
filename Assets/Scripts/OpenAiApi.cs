b07ae0137fc037b014ab82fb2749a6c6ece7c69f
Rob Drury <robjdrewry@gmail.com>
2023-05-17 13:03:35 +0200

    removes api key from source code. adds functionality for reading key from data path. Adds error message if no key is present.
    defaults to null model if no key file is found

--- Assets/Scripts/OpenAiApi.cs
+++ Assets/Scripts/OpenAiApi.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -12,7 +14,6 @@ public class OpenAiApi : MonoBehaviour
 
     public Model model;
 
-    [SerializeField]
     private string apiKey;
 
     private string daVinciUrl = "https://api.openai.com/v1/completions";
@@ -32,15 +33,28 @@ public class OpenAiApi : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        previousMessages = new List<Message>();
-        string prompt = Regex.Replace("I want you to return me a JSON object. All of your output should be a part of the JSON object. Do not output any text except for the JSON object. Here is the example of the JSON object: " + JsonUtility.ToJson(new BattleInfo()) + " \"weapon\" must be one of the following values: \"sword\", \"hammer\", \"bow\". It cannot be anything else. \"size\" must be one of the following values: \"small\", \"medium\", \"large\". It cannot be anything else. Orc names must also include a descriptor starting with \"the\", such as \"Uzguk the Undefeated\". Be creative about these descriptors. Describe the opening scene in the \"openingScene\" string. The opening scene must be a story about an elf about to engage in a battle with a group of orcs. It is the beginning of the story only. Be creative when you come up with descriptions of the orcs. Populate the \"orcs\" array based on the opening scene. ", "\"", "\\\"");
-        isFirstPost = true;
-        isPostInProgress = true;
-        Post(prompt);
+        try
+        {
+            apiKey = new StreamReader(Application.dataPath + "/OpenAiApiKey.txt").ReadToEnd();
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("ERROR: Could not find OpenAiAPI key. Make sure you have added your key to \"thesis_prototype1_Data\\OpenAiApiKey.txt\"  Defaulting to test model.");
+        }
+        finally
+        {
+            previousMessages = new List<Message>();
+            string prompt = Regex.Replace("I want you to return me a JSON object. All of your output should be a part of the JSON object. Do not output any text except for the JSON object. Here is the example of the JSON object: " + JsonUtility.ToJson(new BattleInfo()) + " \"weapon\" must be one of the following values: \"sword\", \"hammer\", \"bow\". It cannot be anything else. \"size\" must be one of the following values: \"small\", \"medium\", \"large\". It cannot be anything else. Orc names must also include a descriptor starting with \"the\", such as \"Uzguk the Undefeated\". Be creative about these descriptors. Describe the opening scene in the \"openingScene\" string. The opening scene must be a story about an elf about to engage in a battle with a group of orcs. It is the beginning of the story only. Be creative when you come up with descriptions of the orcs. Populate the \"orcs\" array based on the opening scene. ", "\"", "\\\"");
+            isFirstPost = true;
+            isPostInProgress = true;
+            Post(prompt);
+        }
     }
 
     public void Post(string prompt)
     {
+        if (apiKey == null) model = Model.test;
+
         switch (model)
         {
             case Model.chatgpt:
